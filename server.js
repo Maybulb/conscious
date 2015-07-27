9c61867fd716cf5fefd0fa2b35f0b05de3fe0326
Josh Trommel <joshtrommel@gmail.com>
2015-07-26 23:13:25 -0700

    Got base shit set up but now I need to get it working

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ var router = express.Router();
 
 var urls = {
   // lastfm's URL is absolutely ridiculous why isn't JSON default wtf
-  "lastfm"   : "",
-  "telegram" : "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks" +
-            "&user=ltrlly&api_key=" + LASTFM_KEY + "&format=json"
-  "github"   : "https://api.github.com/users/lwwws",
-  "echo"     : ""
+  // "telegram"   : "",
+  "lastfm" : "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks" +
+            "&user=ltrlly&api_key=" +  process.env.LASTFM_KEY + "&format=json",
+  "github"   : "http://api.github.com/users/lwwws",
+  // "echo"     : ""
 }
 
 // asssssynchronous  mMMMMMM good shit
@@ -20,22 +20,19 @@ function getPage(url, callback) {
   var request = http.get(url, function(response) {
     var body = ''
 
-    response.on('data', function(chunk) {
-      body += chunk
-    })
-
+    response.on('data', function(chunk) { body += chunk })
     response.on('end', function() { callback(body) })
   })
 }
 
+var json = {
+  "sleeping":false,
+  "last_online":{}
+}
+
 router.get('/', function(req, res) {
-  json = {
-    "awake": false,
-    "last_online": {
-      "telegram": false,
-      "github"  : false,
-      "lastfm"  : false
-    }
+  for (var url in urls) {
+    json.last_online[url] = urls[url];
   }
   res.json(json)
 })
