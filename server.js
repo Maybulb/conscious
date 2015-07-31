09d8c3dbf5b3e9b7dd3ff0f1e339f8b48402b34a
Josh Trommel <joshtrommel@gmail.com>
2015-07-31 16:33:18 -0700

    things work nowgit tag echo nest is so cool

--- server.js
+++ server.js
@@ -15,14 +15,10 @@ var urls = {
   "lastfm"   : "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks" +
             "&user=ltrlly&api_key=" + process.env.LASTFM_KEY + "&format=json",
   "github"   : "https://api.github.com/users/lwwws",
-  "echo"     : "http://developer.echonest.com/api/v4/song/search?api_key=" +
-                process.env.ECHONEST_KEY + "&title=" + "The Fool On The Hill" +
-                "&artist=" + "Beatles" + "&results=1&sort=duration-desc" +
-                "&bucket=audio_summary"
 }
 
 // asssssynchronous  mMMMMMM good shit
-function getPage(url) {
+function get(url) {
   return new Promise(function(resolve, reject) {
     agent.get(url).end(function(err, res) {
       if (err) return reject(err)
@@ -32,14 +28,14 @@ function getPage(url) {
 }
 
 var json = {
-  "sleeping":"i don't know",
+  "sleeping":false,
   "responses":{}
 }
 
 router.get('/', function(req, res) {
   Promise.all(
     Object.keys(urls).map(function(key) {
-      return getPage(urls[key]).then(function(data) {
+      return get(urls[key]).then(function(data) {
         switch(key) {
           case 'lastfm':
             json.responses[key] = data.recenttracks.track[0]
@@ -47,12 +43,21 @@ router.get('/', function(req, res) {
           case 'github':
             json.responses[key] = data.updated_at
             break;
-          case 'echo':
-            json.responses[key] = data
         }
       })
     })
   ).then(function() {
+    var encodedArtist = encodeURIComponent(json.responses.lastfm.artist['#text'])
+    var encodedSong    = encodeURIComponent(json.responses.lastfm.name)
+    var url = "http://developer.echonest.com/api/v4/song/search?api_key=" +
+                  process.env.ECHONEST_KEY + "&title=" + encodedSong +
+                  "&artist=" + encodedArtist + "&results=1&sort=duration-desc" +
+                  "&bucket=audio_summary"
+    console.log(url)
+    return get(url).then(function(data) {
+      json.responses['echo'] = data
+    })
+  }).then(function() {
     res.json(json)
   })
 })
