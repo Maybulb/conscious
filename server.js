06676d96441b71316537c9f66f9a8a0f57b9cdee
Josh Trommel <joshtrommel@gmail.com>
2015-08-03 20:31:59 -0700

    Hopefully this fixes CORS

--- server.js
+++ server.js
@@ -69,5 +69,11 @@ router.get('/', function(req, res) {
   })
 })
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.use('/', router)
 app.listen(port)
